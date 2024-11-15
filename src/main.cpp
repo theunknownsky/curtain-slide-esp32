589950043d52a3a21baef973e9dc57f31ef9ceca
theunknownsky <vonnmamaril24@gmail.com>
2024-11-15 10:40:01 +0800

    led color and led brightness in esp32 now syncs with fbrtdb

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,13 @@
 #define API_KEY "AIzaSyB3ekwAq5EGkH_MsXUCa1R3NxIH7KTK6zk"
 #define DB_URL "https://curtainslide-test-default-rtdb.asia-southeast1.firebasedatabase.app/"
 
-#define LED1 21
+#define LED1 17
+#define LED2 18
+#define LED3 21
 
 bool isWifiConnected;
 
-FirebaseData fbdo, fbdo_1, fbdo_2;
+FirebaseData fbdo_ledStatus, fbdo_ledBrightness, fbdo_ledColorValue, fbdo_curtainState, fbdo_curtainCloseDuration;
 FirebaseAuth auth;
 FirebaseConfig config;
 
@@ -20,9 +22,14 @@ String ctnsld_pword = "12345678";
 String ctnsld_userid = "pOMaE87MRrZD3lg2kyO306XzndR2";
 
 String ledStatusPath = "users/pOMaE87MRrZD3lg2kyO306XzndR2/ledInfo/ledStatus";
+String ledBrightnessPath = "users/pOMaE87MRrZD3lg2kyO306XzndR2/ledInfo/ledBrightness";
+String ledColorValuePath = "users/pOMaE87MRrZD3lg2kyO306XzndR2/ledInfo/ledColorValue";
 
-unsigned long sendDataPrevMillis = 0;
 bool ledStatus;
+int ledBrightness;
+int red;
+int green;
+int blue;
 
 void setup() {
   WiFiManager wm;
@@ -31,6 +38,8 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
   // initializing wifi access point and connection
   isWifiConnected = wm.autoConnect("CTNSLD-69", "testPass");
 
@@ -50,20 +59,66 @@ void setup() {
   Firebase.reconnectWiFi(true);
 
   // stream
-  if(!Firebase.RTDB.beginStream(&fbdo, ledStatusPath)){
-    Serial.printf("Stream Error: %s", fbdo.errorReason().c_str());
+  if(!Firebase.RTDB.beginStream(&fbdo_ledStatus, ledStatusPath)){
+    Serial.printf("Stream Error: %s", fbdo_ledStatus.errorReason().c_str());
   }
+  if(!Firebase.RTDB.beginStream(&fbdo_ledBrightness, ledBrightnessPath)){
+    Serial.printf("Stream Error: %s", fbdo_ledBrightness.errorReason().c_str());
+  }
+  if(!Firebase.RTDB.beginStream(&fbdo_ledColorValue, ledColorValuePath)){
+    Serial.printf("Stream Error: %s", fbdo_ledColorValue.errorReason().c_str());
+  }
+}
+
+void switchLEDStatus(bool status){
+  ledStatus = status;
+  digitalWrite(LED1, ledStatus);
+  Serial.print("LED Status: ");
+  Serial.println(ledStatus);
+}
+
+void changeLEDBrightness(int brightness){
+  ledBrightness = brightness;
+  Serial.print("LED Brightness: ");
+  Serial.println(ledBrightness);
+}
+
+void changeLEDColor(String colorHexcode){
+  long number = (long) strtol(colorHexcode.c_str(), NULL, 16);
+  red = (number >> 16) & 0xFF;
+  green = (number >> 8) & 0xFF;
+  blue = number & 0xFF;
+  Serial.println("LED Color");
+  Serial.print("Red: ");
+  Serial.println(red);
+  Serial.print("Green: ");
+  Serial.println(green);
+  Serial.print("Blue: ");
+  Serial.println(blue);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(Firebase.ready()){
-    if(!Firebase.RTDB.readStream(&fbdo)){
-      Serial.printf("Stream Error: %s", fbdo.errorReason().c_str());
+    if(!Firebase.RTDB.readStream(&fbdo_ledStatus)){
+      Serial.printf("Stream Error: %s", fbdo_ledStatus.errorReason().c_str());
     }
-    if(fbdo.streamAvailable()){
-      ledStatus = fbdo.boolData();
-      digitalWrite(LED1, ledStatus);
+    if(fbdo_ledStatus.streamAvailable()){
+      switchLEDStatus(fbdo_ledStatus.boolData());
+    }
+    if (ledStatus){
+      if(!Firebase.RTDB.readStream(&fbdo_ledBrightness)){
+        Serial.printf("Stream Error: %s", fbdo_ledBrightness.errorReason().c_str());
+      }
+      if(fbdo_ledBrightness.streamAvailable()){
+        changeLEDBrightness(fbdo_ledBrightness.intData());
+      }
+      if(!Firebase.RTDB.readStream(&fbdo_ledColorValue)){
+        Serial.printf("Stream Error: %s", fbdo_ledColorValue.errorReason().c_str());
+      }
+      if(fbdo_ledColorValue.streamAvailable()){
+        changeLEDColor(fbdo_ledColorValue.stringData());
+      }
     }
   }
 }
