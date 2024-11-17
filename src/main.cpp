7afde326e8866c3775e4a719aa7078f55743d404
theunknownsky <vonnmamaril24@gmail.com>
2024-11-17 18:18:26 +0800

    added checkers if curtain is fully opened or closed, added comments for code clarity

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,14 @@
 #include <credentials.h>
 
 
-#define LED1 17
+#define LED1 25
 
-#define SWITCH_CLOSE 18
-#define SWITCH_OPEN 19
+#define SWITCH_CLOSE 35
+#define SWITCH_OPEN 34
 
 bool isWifiConnected;
 
-FirebaseData fbdo_ledStatus, fbdo_ledBrightness, fbdo_ledColorValue, fbdo_curtainState, fbdo_curtainCloseDuration;
+FirebaseData fbdo_ledStatus, fbdo_ledBrightness, fbdo_ledColorValue, fbdo_curtainState, fbdo_curtainCloseDuration, fbdo_isCurtainClosed, fbdo_isCurtainOpened;
 FirebaseAuth auth;
 FirebaseConfig config;
 
@@ -24,8 +24,8 @@ int green;
 int blue;
 int curtainState;
 int curtainCloseDuration;
-int switchOpen;
-int switchClose;
+bool isCurtainOpened;
+bool isCurtainClosed;
 
 
 void setup() {
@@ -35,8 +35,8 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(LED1, OUTPUT);
-  pinMode(SWITCH_CLOSE, INPUT);
-  pinMode(SWITCH_OPEN, INPUT);
+  pinMode(SWITCH_CLOSE, INPUT_PULLUP);
+  pinMode(SWITCH_OPEN, INPUT_PULLUP);
   // initializing wifi access point and connection
   isWifiConnected = wm.autoConnect(AP_SSID, AP_PASSWORD);
 
@@ -68,8 +68,11 @@ void setup() {
   if(!Firebase.RTDB.beginStream(&fbdo_curtainState, CURTAIN_STATE_PATH)){
     Serial.printf("Stream Error: %s", fbdo_curtainState.errorReason().c_str());
   }
-  if(!Firebase.RTDB.beginStream(&fbdo_curtainCloseDuration, CURTAIN_CLOSE_DURATION_PATH)){
-    Serial.printf("Stream Error: %s", fbdo_curtainCloseDuration.errorReason().c_str());
+  if(!Firebase.RTDB.beginStream(&fbdo_isCurtainClosed, IS_CURTAIN_CLOSED_PATH)){
+    Serial.printf("Stream Error: %s", fbdo_isCurtainClosed.errorReason().c_str());
+  }
+  if(!Firebase.RTDB.beginStream(&fbdo_isCurtainOpened, IS_CURTAIN_OPENED_PATH)){
+    Serial.printf("Stream Error: %s", fbdo_isCurtainOpened.errorReason().c_str());
   }
 }
 
@@ -106,49 +109,66 @@ void changeCurtainState(int state){
   Serial.println(curtainState);
 }
 
-void getCurtainCloseDuration(int seconds){
-  curtainCloseDuration = seconds;
-  for(int i = 0; i < curtainCloseDuration; i++){
-    Serial.print(curtainCloseDuration - i);
-    Serial.println(" seconds remaining.");
-    delay(1000);
-  }
-}
-
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Firebase.ready()){
-    if(!Firebase.RTDB.readStream(&fbdo_ledStatus)){
+  if(Firebase.ready()){ // making sure Firebase is ready before calling any firebase function
+    if(!Firebase.RTDB.readStream(&fbdo_ledStatus)){ // reads ledStatus
       Serial.printf("Stream Error: %s", fbdo_ledStatus.errorReason().c_str());
     }
-    if(fbdo_ledStatus.streamAvailable()){
+    if(fbdo_ledStatus.streamAvailable()){ // checks if ledStatus is updated
       switchLEDStatus(fbdo_ledStatus.boolData());
     }
-    if (ledStatus){
-      if(!Firebase.RTDB.readStream(&fbdo_ledBrightness)){
+    if (ledStatus){ // checks if ledStatus is on, the following functions only gets called if ledStatus is on
+      if(!Firebase.RTDB.readStream(&fbdo_ledBrightness)){ // reads ledBrightness
         Serial.printf("Stream Error: %s", fbdo_ledBrightness.errorReason().c_str());
       }
-      if(fbdo_ledBrightness.streamAvailable()){
+      if(fbdo_ledBrightness.streamAvailable()){ // checks if ledBrightness is updated
         changeLEDBrightness(fbdo_ledBrightness.intData());
       }
-      if(!Firebase.RTDB.readStream(&fbdo_ledColorValue)){
+      if(!Firebase.RTDB.readStream(&fbdo_ledColorValue)){ // reads ledColorValue (hexcode)
         Serial.printf("Stream Error: %s", fbdo_ledColorValue.errorReason().c_str());
       }
-      if(fbdo_ledColorValue.streamAvailable()){
+      if(fbdo_ledColorValue.streamAvailable()){ // checks if ledColorValue is updated
         changeLEDColor(fbdo_ledColorValue.stringData());
       }
     }
-    if(!Firebase.RTDB.readStream(&fbdo_curtainState)){
+
+    if(!Firebase.RTDB.readStream(&fbdo_curtainState)){ // reads curtainState
       Serial.printf("Stream Error: %s", fbdo_curtainState.errorReason().c_str());
     }
-    if(fbdo_curtainState.streamAvailable()){
+    if(fbdo_curtainState.streamAvailable()){ // checks if curtainState is updated
       changeCurtainState(fbdo_curtainState.intData());
     }
-    if(!Firebase.RTDB.readStream(&fbdo_curtainCloseDuration)){
-      Serial.printf("Stream Error: %s", fbdo_curtainCloseDuration.errorReason().c_str());
+    if(!Firebase.RTDB.readStream(&fbdo_isCurtainClosed)){ // reads isCurtainClosed
+      Serial.printf("Stream Error: %s", fbdo_isCurtainClosed.errorReason().c_str());
     }
-    if(fbdo_curtainCloseDuration.streamAvailable()){
-      getCurtainCloseDuration(fbdo_curtainCloseDuration.intData());
+    if(fbdo_isCurtainClosed.streamAvailable()){ // checks if isCurtainClosed is updated
+      isCurtainClosed = fbdo_isCurtainClosed.boolData();
+    }
+    if(!Firebase.RTDB.readStream(&fbdo_isCurtainOpened)){ // reads isCurtainOpened
+      Serial.printf("Stream Error: %s", fbdo_isCurtainOpened.errorReason().c_str());
+    }
+    if(fbdo_isCurtainOpened.streamAvailable()){ // checks if isCurtainOpened is updated
+      isCurtainOpened = fbdo_isCurtainOpened.boolData();
+    }
+
+    if(isCurtainClosed != digitalRead(SWITCH_CLOSE)){ // following actions only gets called if the digitalRead(SWITCH_CLOSE) gives a different value 
+      isCurtainClosed = digitalRead(SWITCH_CLOSE);
+      if(!Firebase.RTDB.setBool(&fbdo_isCurtainClosed, IS_CURTAIN_CLOSED_PATH, isCurtainClosed)){ // changes isCurtainClosed in Firebase RTDB
+        Serial.printf("Stream Error: %s", fbdo_isCurtainClosed.errorReason().c_str());
+      }
+      Serial.println("Curtain Closed Status Changing...");
+      Serial.print("Curtain Closed: ");
+      Serial.println(isCurtainClosed);
+    }
+    if(isCurtainOpened != digitalRead(SWITCH_OPEN)){ // following actions only gets called if the digitalRead(SWITCH_OPEN) gives a different value 
+      isCurtainOpened = digitalRead(SWITCH_OPEN);
+      if(!Firebase.RTDB.setBool(&fbdo_isCurtainOpened, IS_CURTAIN_OPENED_PATH, isCurtainOpened)){ // changes isCurtainOpened in Firebase RTDB
+        Serial.printf("Stream Error: %s", fbdo_isCurtainOpened.errorReason().c_str());
+      }
+      Serial.println("Curtain Opened Status Changing...");
+      Serial.print("Curtain Opened: ");
+      Serial.println(isCurtainOpened);
     }
   }
 }
